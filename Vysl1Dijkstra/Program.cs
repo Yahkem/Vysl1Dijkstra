3a074c164e515b335a549db1d026ab811df31265
Richard Kubný <p17083@student.osu.cz>
2018-11-15 12:35:49 +0100

    Add project files.

---
+++ Vysl1Dijkstra/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vysl1Dijkstra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
